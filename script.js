b7dc736a2dacf1b745db5217c63c35190505b265
unknown <ben.jervis.keele@googlemail.com>
2022-12-05 11:56:08 +0000

    new password variable created

--- script.js
+++ script.js
@@ -88,26 +88,52 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-//These are my variables created using the given arrays above 
+//These are the variables I have created using the given arrays above 
 
 var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var upperChar =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numericChar = ["0","1","2","3","4","5","6","7","8","9"];
 var specialChar = ["!","$","^","&","*","-",",","=","+","_","?"]
 
-//Variable created as an array to store the choices the user inputs 
-var userPassword = [];
+//Variable created as an empty array to store the choices the user inputs 
+var newUserPassword = [];
 
-var password = "";
+                                //---User options---//
+
+//User choices - This allows the user to chose different options using prompts, if statements and confirms 
 
 
 // Function to prompt user for password options
-function getPasswordOptions() {
-  //passwordLength = prompt ("please enter the length of your required password");
-  //lowerCasedCharacters = prompt ("please enter a lower case letter");
-  //upperCasedCharacters = prompt ("please enter a upper case letter");
-  //numericCharacters = prompt("please enter a number 1-9");
-  //specialCharacters = prompt("please enter a special character")
+//function getPasswordOptions() {
+ 
+    var passWordlength =  parseInt(prompt("Please choose a password between 10 and 64 characters"));
+        
+     if (passWordlength > 10 ); {
+        alert("Please be aware: All passwords must be between 10-64 characters.");
+        prompt("How many characters do you want to be in your password?");
+    
+       }
+    upperChar = confirm("Click ok to confirm including upper case letters."); 
+    lowerChar = confirm("Click ok to confirgim including lower case letters."); 
+    numericChar = confirm("Click ok to confirm including numeric values."); 
+    specialChar = confirm("Click ok to confirm including special characters."); 
+    
+    if (upperChar === true) {  // Here I have used the .push method to add all user choices to the new password variable 
+        (newUserPassword.push(caps));
+     }
+      if (lowerChar === true) {
+         (newUserPassword.push(lower));
+      }
+     if (numericChar === true) {
+         (newUserPassword.push(numeric));
+      }
+     if (specialChar = true) {
+         (newUserPassword.push(special));
+      }
+     
+     var newPassWordstring = newUserPassword.join(""); //I have used .join to join all the characters entered together
+      
+     var password = "";
 
 
 }
